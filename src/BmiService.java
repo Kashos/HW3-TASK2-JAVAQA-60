5e667ddd8dfde2de9f0886e4e4b090accbb898a1
Egor Kachesov <kashos152@gmail.com>
2023-04-09 16:25:11 +0700

    modified BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-        public double calculate (int a, double b) {
-            int b1 = 2; //Степень для метода Math.pow
-            double height_m2 = Math.pow(b, b1);
-            double result =  a / height_m2 ; //Расчт бонусных миль
-            return result;
-        }
+    public double calculate(int weight, double height) {
+        int degree = 2; //Степень для метода Math.pow
+        double height_m2 = Math.pow(height, degree);
+        double result = weight / height_m2; //Расчт бонусных миль
+        return result;
     }
+}
